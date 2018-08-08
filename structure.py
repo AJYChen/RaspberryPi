a669459e38b26e014e996e246b05de94d54704cd
Jun Yu Chen <chinesetaipeitw@gmail.com>
2018-08-08 15:08:18 +0800

    add structure'

---
+++ structure.py
@@ -0,0 +1,15 @@
+#60
+seconds_per_day = 86400 # 60 sec
+#.....
+#.....
+#.....
+
+alphabet = '';
+alphabet += 'abcdefg'
+alphabet += 'hijklmnop'
+
+alpyabet = 'abcdefg' + \
+           'hijklmnop' + \
+           'qrstuv' + \
+           'wxyz'
+print(alpyabet)
