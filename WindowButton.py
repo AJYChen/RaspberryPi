bf22085f169ef0320eaca747dbe0808ee7eed558
Jun Yu Chen <chinesetaipeitw@gmail.com>
2018-08-17 14:23:30 +0800

    Add new

--- WindowButton.py
+++ WindowButton.py
@@ -5,8 +5,8 @@ from tkinter import *
 def createWindow(window):
      xf = Frame(window, relief=GROOVE, borderwidth=2);
      Label(xf, text = "這是LED控制區").pack(pady=10,padx = 100);
-     Button(xf, text = "LED CLOSE").pack(side=LEFT);
-     Button(xf, text = "LED OPEN").pack(side=LEFT);
+     Button(xf, text = "LED CLOSE").pack(side=LEFT, expand = YES, fill = X);
+     Button(xf, text = "LED OPEN").pack(side=LEFT,expand = YES, fill = X);
      xf.pack(pady=10, padx = 10);
 
 
@@ -16,16 +16,3 @@ if __name__ == "__main__":
     createWindow(root);
     root.mainloop();
 
-from tkinter import *
-
-def createWindow(window):
-    xf = Frame(window, relief = GROOVE,borderwidth = 2)
-    Label(xf,text = "This is the LED control area").pack(padx = 10, pady = 100)
-    xf.pack(pady = 100, padx = 10)
-    
-if __name__ == "__main__":
-    
-
-    root = Tk()
-    root.title("New Window")
-    createWindow(root)
